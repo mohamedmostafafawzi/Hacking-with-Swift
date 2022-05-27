1423b0d1104f55947dfbeca862b6ee32618a51c1
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-05-27 18:35:26 +0200

    Create Project1
    Project 1: Storm Viewer
    Constants and variables, UITableView, UIImageView, FileManager,
    storyboards
    Get started coding in Swift by making an image viewer app and
    learning key concepts.

---
+++ UIKit/project1/project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Mohamed Fawzi on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
