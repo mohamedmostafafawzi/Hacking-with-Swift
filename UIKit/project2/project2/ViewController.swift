208c4861a359ef41ac2f9ce2e28559f79afbc11a
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-05-28 13:18:02 +0200

    From outlets to actions: creating an IBAction
    Add action to the buttons, tags, game logic and alert to display with
    if the answer is correct or not and the score.
    
    [uikit-project2]

--- UIKit/project2/project2/ViewController.swift
+++ UIKit/project2/project2/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
     }
 
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -44,6 +44,23 @@ class ViewController: UIViewController {
         
         title =  countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String?
+        
+        if sender.tag == correctAnswer {
+            title =  "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
     
 }
 
