b8f755c85aa985d83a22d9ede9d5711c848fa096
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-06-05 15:40:30 +0200

    Setting up
    Create project5
    
    [uikit-project5]

---
+++ UIKit/project5/project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project5
+//
+//  Created by Mohamed Fawzi on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
