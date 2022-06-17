a3e880264e02d57801c4ea7d0de1dbd22686640d
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-06-17 17:26:18 +0200

    Setting up
    Project 7: Whitehouse Petitions
    
    [uikit-project7]

---
+++ UIKit/project7/project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Mohamed Fawzi on 6/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
