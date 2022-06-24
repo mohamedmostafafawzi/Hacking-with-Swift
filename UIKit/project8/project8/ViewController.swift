1bda2533f47a91ac0db2d38ed2fcd7930a93ada9
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-06-25 00:15:40 +0200

    Setting up
    [uikit/Project8]

---
+++ UIKit/project8/project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by Mohamed Fawzi on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
