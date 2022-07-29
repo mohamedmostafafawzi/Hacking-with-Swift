b48a5a6e1568e1283ac29382f804d9c61c778652
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-07-30 00:05:37 +0200

    Setting up
    [uikit/Project10]

---
+++ UIKit/project10/project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by Mohamed Fawzi on 7/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
