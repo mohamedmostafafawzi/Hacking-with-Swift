dc440494014d092b25dc7d76aed50240d0e7f1d9
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-09-18 23:17:08 +0200

    Setting up
    [uikit/Project12]

---
+++ UIKit/project12b/project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Mohamed Fawzi on 7/30/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
