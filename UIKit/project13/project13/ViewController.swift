8be10253a8faedede7a85e08433c40464c3c8710
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2022-09-23 20:47:34 +0200

    Setting up
    [uikit/Project13]

---
+++ UIKit/project13/project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project13
+//
+//  Created by Mohamed Fawzi on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
